91b57edd936ac3ede4b58e3f357f45eded2b545d
protihw <bernardo.proti@hotmail.com>
2025-04-07 17:05:29 -0300

    Ignite Feed

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,11 +1,21 @@
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 import { Post } from "./components/Post"
-import "./styles.css"
+import styles from './App.module.css'
+import "./global.css"
 
 export function App() {
   return (
     <div>
-      <Post author="Bernardo Proti" content="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
-      <Post author="Lucas Miranda" content="Um novo post legal sobre o que fiz hoje."/>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post author="Bernardo Proti" content="Lorem ipsum dolor sit amet consectetur adipisicing elit." />
+          <Post author="Lucas Miranda" content="Um novo post legal sobre o que fiz hoje." />
+        </main>
+      </div>
     </div>
   )
 }
