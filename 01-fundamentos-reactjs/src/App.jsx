f45dcd7a3c6ff336f2ccfb0e99deba6c131cdd85
protihw <bernardo.proti@hotmail.com>
2025-04-16 16:28:35 -0300

    Add: Dynamic post content

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -4,16 +4,54 @@ import { Post } from "./components/Post"
 import styles from './App.module.css'
 import "./global.css"
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://www.github.com/bernardoproti.png",
+      name: "Bernardo Proti",
+      role: "Web Developer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: "link", content: "jane.design/doctorcare"},
+    ],
+    publishedAt: new Date("2025-04-16 15:10:00")
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://www.github.com/gianlucasampogna.png",
+      name: "Gianluca Sampogna",
+      role: "Web Developer"
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: "link", content: "jane.design/doctorcare"},
+    ],
+    publishedAt: new Date("2025-04-11 15:06:00")
+  },
+];
+
 export function App() {
   return (
     <div>
       <Header />
 
       <div className={styles.wrapper}>
-        <Sidebar/>
+        <Sidebar />
         <main>
-          <Post/>
-          <Post/>
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
