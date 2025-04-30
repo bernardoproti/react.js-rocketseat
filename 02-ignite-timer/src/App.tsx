c893f2f97207e3d711daaa53a5ca60e29f84f0d5
protihw <bernardo.proti@hotmail.com>
2025-04-30 16:53:36 -0300

    feat: themes and styled components

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,13 +1,17 @@
-import { Button } from './components/Button'
+import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button';
+
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
   return (
-    <>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="sucess" />
-      <Button variant="danger" />
+    <ThemeProvider theme={defaultTheme}>
       <Button />
-    </>
+      <Button />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
--- 02-ignite-timer/src/components/Button.styles.ts
+++ 02-ignite-timer/src/components/Button.styles.ts
@@ -1,23 +1,12 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
-export type ButtonVariant = "primary" | "secondary" | "sucess" | "danger";
-
-interface ButtonContainerProps {
-  variant: ButtonVariant;
-}
-
-const buttonVariants = {
-  primary: "purple",
-  secondary: "orange",
-  danger: "red",
-  sucess: "green",
-}
-
-export const ButtonContainer = styled.button<ButtonContainerProps>`
+export const ButtonContainer = styled.button`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: .5rem;
 
-  ${props => {
-    return css`background-color: ${buttonVariants[props.variant]}`
-  }}
+  background-color: ${props => props.theme['green-500']};
+  color: ${props => props.theme.white};
 `
