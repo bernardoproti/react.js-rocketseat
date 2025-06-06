364ef2a3441f526b4091717277b725ac68a96fd9
protihw <bernardo.proti@hotmail.com>
2025-06-06 16:16:29 -0300

    feat: routes with their layout

---
+++ 04-pizza-shop/postcss.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // Mudei de 'tailwindcss' para '@tailwindcss/postcss'
+    autoprefixer: {},
+  },
+};
--- 04-pizza-shop/vite.config.ts
+++ 04-pizza-shop/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000, 
+    port: 3000,
     open: true,
   },
-})
+});
