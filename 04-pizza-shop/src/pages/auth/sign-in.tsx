45a2e18f18534f50e04d6cce45ac55a8f5b7cc0e
protihw <bernardo.proti@hotmail.com>
2025-06-09 16:46:01 -0300

    style: sign-in page

--- 04-pizza-shop/src/pages/auth/sign-in.tsx
+++ 04-pizza-shop/src/pages/auth/sign-in.tsx
@@ -1,3 +1,35 @@
+import { Helmet } from "react-helmet-async";
+import { Button } from "../../components/ui/button";
+import { Label } from "../../components/ui/label";
+import { Input } from "../../components/ui/input";
+
 export function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <>
+      <Helmet title="Sign In" />
+      <div className="p-8">
+        <div className="w-[350px] flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-small text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mal</Label>
+              <Input id="email" type="email"></Input>
+            </div>
+
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
