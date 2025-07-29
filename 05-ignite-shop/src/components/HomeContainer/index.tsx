c4713fdd15368aae68a35fb6fd1a5ccf0e87def5
protihw <bernardo.proti@hotmail.com>
2025-07-29 16:00:39 -0300

    trying to fix carousel

--- 05-ignite-shop/src/components/HomeContainer/index.tsx
+++ 05-ignite-shop/src/components/HomeContainer/index.tsx
@@ -6,7 +6,7 @@ import shirt1 from "@/assets/shirts/1.png";
 import shirt2 from "@/assets/shirts/2.png";
 import shirt3 from "@/assets/shirts/3.png";
 import { useKeenSlider } from "keen-slider/react";
-import "keen-slider/keen-slider.min.css";
+import "keen-slider/keen-slider.css";
 
 export function HomeContainer() {
   const [sliderRef] = useKeenSlider({
@@ -17,21 +17,21 @@ export function HomeContainer() {
 
   return (
     <Container ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide number-slide1">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} width={520} height={480} alt="Camiseta X" />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product className="keen-slider__slide number-slide2">
+      <Product className="keen-slider__slide">
         <Image src={shirt2} width={520} height={480} alt="Camiseta Y" />
         <footer>
           <strong>Camiseta Y</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product className="keen-slider__slide number-slide3">
+      <Product className="keen-slider__slide">
         <Image src={shirt3} width={520} height={480} alt="Camiseta Z" />
         <footer>
           <strong>Camiseta Z</strong>
